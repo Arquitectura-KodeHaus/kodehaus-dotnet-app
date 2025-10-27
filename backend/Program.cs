f0b2e31cdcbb44a8b399ee222c7f47f057d35c55
Juan Diego Arias Duran <63438568+ElAreaAl2@users.noreply.github.com>
2025-10-26 21:15:56 -0500

    Add CORS origin for production frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200",
+                "https://kodehaus-frontend-dotnet-616328447495.us-central1.run.app")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
