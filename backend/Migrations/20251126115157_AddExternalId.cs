ffd248d72f2daf11a815dc815cbf18aaae89a54b
Arkan56 <seb4s.s44@gmail.com>
2025-11-26 09:38:42 -0500

    Fix: communication with java-0 service to create local

---
+++ backend/Migrations/20251126115157_AddExternalId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExternalId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdPlaza",
+                table: "Locales",
+                newName: "ExternalId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExternalId",
+                table: "Locales",
+                newName: "IdPlaza");
+        }
+    }
+}
